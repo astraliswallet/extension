0c4a311812f1f63e94ccc461e659f7c77094d4ba
Abhinav Srivastava <atg271@gmail.com>
2023-07-03 01:39:53 +0530

    wip

--- src/Components/networkSelector.js
+++ src/Components/networkSelector.js
@@ -4,24 +4,23 @@ import { useAtom } from 'jotai';
 import { devnetURL, testnetURL, mainnetURL } from '../constants';
 import { networkRPCAtom } from '../atoms';
 
-const networkSelector = () => {
-    const [selectedAtom, setSelectedAtom] = useAtom(devnetURL);
-  
-    const handleAtomChange = (event) => {
-      setSelectedAtom(event.target.value);
-    };
-  
-    return (
-      <Box>
-        <Text>Selected Atom: {selectedAtom}</Text>
-        <Select value={selectedAtom} onChange={handleAtomChange} mt={4} w="200px">
-          <option value={devnetURL}>Devnet</option>
-          <option value={testnetURL}>Testnet</option>
-          <option value={mainnetURL}>Mainnet</option>
-        </Select>
-      </Box>
-    );
+const NetworkSelector = () => {
+  const [selectedAtom, setSelectedAtom] = useAtom(networkRPCAtom);
+
+  const handleAtomChange = (event) => {
+    setSelectedAtom(event.target.value);
   };
-  
-  export default networkSelector;
-  
+
+  return (
+    <Box>
+      <Text>Selected Atom: {selectedAtom}</Text>
+      <Select value={selectedAtom} onChange={handleAtomChange} mt={4} w="200px">
+        <option value={devnetURL}>Devnet</option>
+        <option value={testnetURL}>Testnet</option>
+        <option value={mainnetURL}>Mainnet</option>
+      </Select>
+    </Box>
+  );
+};
+
+export default NetworkSelector;
