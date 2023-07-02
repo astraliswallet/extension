a789dd12a025a96ea2c6f5a8877d62a656508e46
Abhinav Srivastava <atg271@gmail.com>
2023-07-02 20:49:07 +0530

    wip

--- src/atoms.js
+++ src/atoms.js
@@ -4,4 +4,4 @@ import { atomWithStorage } from 'jotai/utils';
 export const walletCreatedAtom = atomWithStorage(false);
 export const walletAtom = atomWithStorage('wallet', 'jasnsnc');
 export const topupBalanceAtom = atomWithStorage('topupBalance', null);
-export const privateKey  = atomWithStorage('privateKey', null);
+export const privateKeyAtom  = atomWithStorage('privateKey', null);
